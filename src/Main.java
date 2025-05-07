fa50a166e0ea07b7e8ebe7478c7091fa7be908ab
Bion Ullenius <Bion.Holmberullenius@elev.ga.ntig.se>
2025-05-07 12:55:39 +0200

    färdig med att skriva av från dokumentet

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         ServerSocket server = null;
-        Socket client;
+        Socket klient;
 
         //Standard port nummer vi ska använda
         int portnummer = 55050;
@@ -22,6 +22,43 @@ public class Main {
             System.exit(1);
         }
 
-        //wait for the 
+        //vänta på data från klienten och svara.
+        while (true){
+            try{
+                //väntar på att en anslutning till socketen sker och accepterar den.
+                // Denna metoden blockerar tills en anslutning är gjord
+                System.out.println("Väntar på anslutnings förfrågan...");
+                klient = server.accept();
+
+                System.out.println("Anslutnings förfrågan är accepterad...");
+                String klientHost = klient.getInetAddress().getHostAddress();
+                int klientport = klient.getPort();
+                System.out.println("Klient host = " + klientHost + "Klient port = " + klientport);
+
+                //Läs data från klienten
+                InputStream klientIn = klient.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(klientIn));
+                String medFrnKlient = br.readLine();
+                System.out.println("Meddelande mottaget från klienten: " + medFrnKlient);
+
+                //Skicka svar till klienten
+                if (medFrnKlient != null && ! medFrnKlient.equalsIgnoreCase("bye")) {
+                    OutputStream klientOut = klient.getOutputStream();
+                    PrintWriter pw = new PrintWriter(klientOut, true);
+                    String ansMsg = "Hello, " + medFrnKlient;
+                    pw.println(ansMsg);
+                }
+
+                //Stänga sockets
+                if (medFrnKlient != null && medFrnKlient.equalsIgnoreCase("bye")){
+                    server.close();
+                    klient.close();
+                    break;
+                }
+
+            } catch (IOException ie){
+                System.out.println("ERROR, Något gick FEL.");
+            }
+        }
     }
 }
